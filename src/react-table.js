b5c1c4ffbdc0513f90a468df45be886f07466ce4
Ezell <lynellf@gmail.com>
2019-09-27 11:12:36 -0500

    Create react-table.js

---
+++ src/react-table.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './app'
+
+class ReactTable extends HTMLElement {
+  connectedCallback() {
+    const id = this.id
+    const mountPoint = document.createElement('div')
+    this.attachShadow({ mode: 'open' }).appendChild(mountPoint)
+
+    ReactDOM.render(<App id={id} />, mountPoint)
+  }
+}
+
+customElements.define('react-table', ReactTable)
