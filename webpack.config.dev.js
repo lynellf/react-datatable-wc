dafd4dbb75811dccaa3c9f3d096efe1baf3ba84d
Ezell <lynellf@gmail.com>
2019-11-13 10:34:44 -0600

    create webpack dev config

---
+++ webpack.config.dev.js
@@ -0,0 +1,37 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const merge = require('webpack-merge')
+const common = require('./webpack.config.common')
+
+module.exports = merge(
+  {
+    mode: 'development',
+    watch: true,
+    devtool: 'inline-source-map',
+    output: {
+      path: `${__dirname}/dev`,
+      filename: 'bundle.[hash].js',
+      // publicPath: '/public'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(html)$/,
+          use: {
+            loader: 'html-loader',
+            options: {
+              attrs: [':data-src']
+            }
+          }
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: `${__dirname}/index.html`
+      }),
+      new CleanWebpackPlugin()
+    ]
+  },
+  common
+)
