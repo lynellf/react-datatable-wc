16bc9be00bcad6e6fd6d55342c0d235f2584d3e5
Ezell <lynellf@gmail.com>
2019-11-13 10:30:09 -0600

    add react imports

--- src/react-table.js
+++ src/react-table.js
@@ -1,3 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Styles from './styles'
+import App from './app'
+
 class ReactTable extends HTMLElement {
   constructor() {
     super()
